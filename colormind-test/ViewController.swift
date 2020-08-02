ce34bcd805ef1b919bae73c4fab69383bc20f2ec
Rudolf <paduraru.rudolf.vasile@gmail.com>
2020-08-02 10:53:40 +0300

    Initial Commit

---
+++ colormind-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  colormind-test
+//
+//  Created by Rudolf Paduraru on 02/08/2020.
+//  Copyright © 2020 Rudolf Paduraru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
